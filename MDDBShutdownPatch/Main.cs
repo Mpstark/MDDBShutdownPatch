a2cc7d787085444efeb79c5f12626d6ff09b9f7c
Michael P. Starkweather <mpstark@gmail.com>
2019-02-01 22:57:44 -0600

    First working prototype

---
+++ MDDBShutdownPatch/Main.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Harmony;
+
+// ReSharper disable UnusedMember.Global
+
+namespace MDDBShutdownPatch
+{
+    public static class Main
+    {
+        public static void Init()
+        {
+            var harmony = HarmonyInstance.Create("io.github.mpstark.MDDBShutdownPatch");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ MDDBShutdownPatch/Patches/UnityGameInstance.cs
@@ -0,0 +1,17 @@
+﻿using BattleTech.Data;
+using Harmony;
+
+// ReSharper disable UnusedMember.Global
+// ReSharper disable InconsistentNaming
+
+namespace MDDBShutdownPatch.Patches
+{
+    [HarmonyPatch(typeof(MetadataDatabase), "Shutdown")]
+    public static class MetadataDatabase_Shutdown_Patch
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
